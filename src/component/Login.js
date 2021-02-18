2758512c49369a21e6c5c2dabde4d484f8df4a12
artgallery <nirvikar21@gmail.com>
2021-02-18 18:20:47 +0530

    sds

--- src/component/Login.js
+++ src/component/Login.js
@@ -6,6 +6,7 @@ import Header from './Header.js';
 const Login=()=>{
   const[email,setEmail]=useState();
   const[password,setPassword]=useState();
+  const[error,setError]=useState();
   const history=useHistory("");
   useEffect(()=>{
     if(localStorage.getItem('user-info')){
@@ -14,6 +15,26 @@ const Login=()=>{
   });
 
   function Login(){
+    
+        const emailObj = document.getElementById('email')
+        const passwordObj = document.getElementById('password')
+        if(emailObj.value==''){
+          emailObj.classList.add('border-danger');
+          
+        }else{
+          emailObj.classList.remove('border-danger');
+
+        }
+        if(passwordObj.value==''){
+          passwordObj.classList.add('border-danger');
+          
+
+        }else{
+          passwordObj.classList.remove('border-danger');
+        }
+    if(emailObj.value== "" || passwordObj.value == ""){
+      return ;
+    }
     console.log(email,password);
   var formBody = [];
   formBody=["email="+email, "password="+password];
@@ -26,15 +47,21 @@ const Login=()=>{
         body: formBody
         
     }).then((resp)=>{
-        resp.json().then((result)=>{
+      console.error("=======1",resp);
+        if(resp.status==200){
+          resp.json().then((result)=>{
             console.log("--=",result.token); 
             if(result.token){
               localStorage.setItem("user-info",JSON.stringify(result));
-               history.push('/home');
+              history.push('/home');
             } 
         })
-    })
-    
+      }else{
+        setError("user not found");
+      }
+     })
+    console.log(error);
+
     
  }
   return(
@@ -43,10 +70,16 @@ const Login=()=>{
   
   
       <div className="col-sm-6 offset-sm-3">
+        {error!=undefined?
+          <div className="alert alert-danger">
+            <strong>Danger!</strong> {error}
+          </div>
+          :''
+        }
           <h1>Login Page</h1>
-          <input type="text" className="form-control" name="email" onChange={(e)=>setEmail(e.target.value
+          <input type="text" className="form-control" name="email" id="email" onChange={(e)=>setEmail(e.target.value
           )} placeholder="Email"/><br/>
-          <input type="password" className="form-control" onChange={(e)=>setPassword(e.target.value
+          <input type="password" id="password" className="form-control" onChange={(e)=>setPassword(e.target.value
           )} placeholder="Password"/><br/>   
           <button name="btn" className="btn btn-primary" onClick={Login}>Submit</button>
       </div>
